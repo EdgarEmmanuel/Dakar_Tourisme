0a3647fcff8612382f80b02f9cbf023968459664
Edgar <mmnledgar@gmail.com>
2020-07-14 09:06:20 +0000

    Realisation de la page Contribute et gestion du routage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContributePageComponent } from './contribute-page/contribute-page.component';
+import { DestinationComponent } from './destination/destination.component';
 
 
 
 const routes: Routes = [
+  {path:'',component:ContributePageComponent,pathMatch:'full'},
   {path:"contribute",component:ContributePageComponent},
+  {path:"destination",component:DestinationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContributePageComponent } from './contribute-page/contribute-page.component';
+import { DestinationComponent } from './destination/destination.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContributePageComponent,
+    DestinationComponent,
+    BlogComponent,
   ],
   imports: [
     BrowserModule,
