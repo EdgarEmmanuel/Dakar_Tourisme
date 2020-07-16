807c3ab83ea81c4b0fed416ce276b1aadd219532
Edgar <mmnledgar@gmail.com>
2020-07-16 09:04:28 +0000

    Gestion du menu et de la page destination

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
 import { ContributePageComponent } from './contribute-page/contribute-page.component';
 import { DestinationComponent } from './destination/destination.component';
 
@@ -9,7 +9,8 @@ import { DestinationComponent } from './destination/destination.component';
 const routes: Routes = [
   {path:'',component:ContributePageComponent,pathMatch:'full'},
   {path:"contribute",component:ContributePageComponent},
-  {path:"destination",component:DestinationComponent}
+  {path:"destination",component:DestinationComponent},
+  {path:'blog',component:BlogComponent},
 ];
 
 @NgModule({
